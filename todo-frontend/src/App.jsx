b3c19c05c744e2765d526dbcc829439229b9cefe
Flavia Onofrei <flaviaonofrei@Flavias-MacBook-Air.local>
2026-01-12 23:37:47 +0200

    use vite_api_url in prod

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
-const API = "http://localhost:5050";
+const API = import.meta.env.VITE_API_URL || "http://localhost:5050";
 
 const FILTERS = [
   { key: "all", label: "All" },
